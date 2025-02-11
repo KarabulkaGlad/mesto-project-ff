08c364987496721436560383966435b2cd4dd797
Vladislav <Karabulka.glad@yandex.ru>
2025-02-11 21:25:07 +0400

    api config

---
+++ src/scripts/api.js
@@ -0,0 +1,19 @@
+const config = {
+    baseUrl: 'https://nomoreparties.co/v1/wff-cohort-31',
+    headers: {
+      authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
+      'Content-Type': 'application/json'
+    }
+  }
+  
+  export const getInitialCards = () => {
+    return fetch(`${config.baseUrl}/cards`, {
+      headers: config.headers
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
+  } 
